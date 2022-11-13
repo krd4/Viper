dc33024e65d50321fa762ca4914e8137207509c4
kryousuke <k000ryousuke@gmail.com>
2022-11-13 17:16:38 +0900

    Change cstdio to iostream

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,25 @@
-int main() {
-    
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int run(string file_name) {
+    cout << file_name << endl;
+    return 0;
+}
+
+int run_prompt() {
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        cout << "Usage: viper [script]" << endl;
+        return 1;
+    } else if (argc == 2) {
+        string file_name(argv[1]);
+        run(file_name);
+    }
+
+    run_prompt();
 }
